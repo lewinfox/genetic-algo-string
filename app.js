f2a19ed135ea39fa021c01a7d02dd959b52af3e6
Lewin Appleton-Fox <lewin.a.f@gmail.com>
2018-12-26 00:26:59 +0000

    Add max_fitness display to console output

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const target = 'good morning lord nelson';
-const population_size = 200;
+const target = 'round and round the ragged rock';
+const population_size = 100;
 
-let p = new Population(population_size, target);
+let p = new Population(population_size, target, 0.01, 1);
 // const original_p = JSON.parse(JSON.stringify(p));
 // console.log("Original object:");
 // console.log(original_p);
@@ -10,11 +10,13 @@ let counter = 0;
 
 // Main loop
 while (p.best_candidate.genome_as_string !== target && counter < 10000) {
-  console.log(`Iteration: ${counter} \t ${p.best_candidate.genome_as_string}`);
+  if (counter % 1000 == 0 || (counter % 10 == 0 && counter < 1000)) {
+    console.log(`Iteration: ${counter} \t ${p.best_candidate.genome_as_string}\tMax fitness: ${p._max_fitness}`);
+  }
   p.calculate_fitness(target);
   const original_p = JSON.parse(JSON.stringify(p));
-  // console.log(original_p);
   p.breed();
   counter ++;
 }
-console.log(`Iteration: ${counter} \t ${p.best_candidate.genome_as_string}`);
+console.log(`Completed on iteration: ${counter} \t ${p.best_candidate.genome_as_string}`);
+console.log(p);
